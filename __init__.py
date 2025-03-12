df7c146d6bc312f2acc59f0d5dcf2651f8abfc80
Divya Poojari <157617863+pdivya26@users.noreply.github.com>
2025-03-12 21:42:54 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-# Configure SQLite Database
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from app import app  # Import the app instance
+from app import *  # Import after app and db are defined
